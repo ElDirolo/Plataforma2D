81c3f7d158ef784dd4f96d2e7a171e56006d727a
ElDirolo <rosaldidac@gmail.com>
2022-10-05 11:00:43 +0200

    muerte

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public static bool isGrounded;
+    
+    // Start is called before the first frame update utilizando trigers o hacerlo con el caracther controler de jugeo 3d
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,29 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerMove : MonoBehaviour
 {
     public float speed = 5;
-    private float horizontal;
-    private Transform playertrasnform;
+    public float jumpForce = 5;
+
     private Rigidbody2D rigided;
+    private Animator anim;
+
+    private float horizontal;
+
+    public PlayableDirector director;
+    //private Transform playertrasnform;
     
-    // Start is called before the first frame update
+
     void Start()
     {
-        playertrasnform = GetComponent<Transform>();
+        //playertrasnform = GetComponent<Transform>();
         rigided = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        //if(GroundChek.isGrounded){}
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        
+
+        if(horizontal == 0)
+        {
+            anim.SetBool("Correr", false);
+        }
+        if(horizontal == 1)
+        {
+            transform.rotation = Quaternion.Euler(0,0,0);
+            anim.SetBool("Correr", true);
+        }
+        if(horizontal == -1)
+        {
+            transform.rotation = Quaternion.Euler(0,180,0);
+            anim.SetBool("Correr", true);
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigided.velocity = new Vector2(rigided.velocity.x, jumpForce);
+        }
         //playertrasnform.position += new Vector3 (horizontal * speed * Time.deltaTime, 0, 0);
         //playertrasnform.position += new Vector3 (1, 0, 0) * horizontal * speed * Time.deltaTime;
         //playertrasnform.Translate(Vector3.right * horizontal * speed * Time.deltaTime, Space.World);
+    }
+
+    void FixedUpdate()
+    {
         rigided.velocity = new Vector2(horizontal * speed, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Cinematica")
+        {
+            director.Play();
+        }
+        
     }
 }
